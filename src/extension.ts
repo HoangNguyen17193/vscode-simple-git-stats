71ac000443d137f046d380aeff8aae95debb9c54
HoangNguyen17193 <hoangnh17193@gmail.com>
2019-07-12 09:37:44 +0700

    load charJS from local instead of CDN

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 import Controller from './controllers/mainController';
 
 export function activate(context: vscode.ExtensionContext) {
-  const controller = new Controller();
+  const controller = new Controller(context);
   let disposable = vscode.commands.registerCommand("GitStats.viewCommits",() => {
     controller.showCommitsPanel();
   }
